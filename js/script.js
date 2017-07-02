8229c739b85b7bb7c7b996020e9fcc921908303d
yungpanko <jrjohns@mit.edu>
2017-07-02 10:36:18 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   // toggle and delete
-  $('.course').on('click', function(){
-    $(this).find('.detail').slideToggle();
-  });
-
-  $('.course .delete').on('click', function(){
-    alert("about to delete");
-  });
-
-  //stop propagation
   // $('.course').on('click', function(){
   //   $(this).find('.detail').slideToggle();
   // });
-
-  // $('.course .delete').on('click', function(event){
+  //
+  // $('.course .delete').on('click', function(){
   //   alert("about to delete");
-  // event.stopPropagation();
-
   // });
+
+  //stop propagation
+  $('.course').on('click', function () {
+    $(this).find('.detail').slideToggle();
+  });
+
+  $('.course .delete').on('click', function (event) {
+    alert("about to delete");
+    event.stopPropagation();
+
+  });
 });
